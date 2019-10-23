95132efb168a759a0d04778b8ffaafe310e04531
artem1989h <a.habinskyi@gmail.com>
2019-10-23 23:01:19 +0300

    install npm, bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import './App.css';
+import Weather from './app_component/weather.component';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Weather = () =>{
+    return(
+        <div className="conteiner">
+            <div className="cards">
+                <h1>Lviv</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Weather;
