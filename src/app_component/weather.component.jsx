f1b5391c2dbd44209021fd1e630a45664113f457
artem1989h <a.habinskyi@gmail.com>
2019-10-23 23:26:10 +0300

    add function minmaxTemp

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -6,9 +6,23 @@ const Weather = () =>{
             <div className="cards">
                 <h1>Lviv</h1>
                 <h5 className="icons"></h5>
-                <i className="wi wi-day-sunny"></i>
+                <i className="wi wi-day-sunny display-1"></i>
+                <h1 className="temperature">25&deg;</h1>
+
+                {minmaxTemp(15,20)}
             </div>
         </div>
+    );
+}
+
+
+function minmaxTemp(min,max){
+    return(
+        <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+        </h3>
+        
     );
 }
 
