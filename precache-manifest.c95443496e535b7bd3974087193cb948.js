ad8caf1a5cdc1b133556c45eef193b27a1e9af40
artem1989h <a.habinskyi@gmail.com>
2019-10-27 22:52:34 +0200

    Updates

---
+++ precache-manifest.c95443496e535b7bd3974087193cb948.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ce1dfee9845d47355279c0c59c472e",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "255056fd854eb50bef34",
+    "url": "/weather/static/css/2.a1f3ff76.chunk.css"
+  },
+  {
+    "revision": "71ae44977d2ebba235b2",
+    "url": "/weather/static/css/main.a7e0f8bf.chunk.css"
+  },
+  {
+    "revision": "255056fd854eb50bef34",
+    "url": "/weather/static/js/2.d2276fb7.chunk.js"
+  },
+  {
+    "revision": "71ae44977d2ebba235b2",
+    "url": "/weather/static/js/main.b71c34f9.chunk.js"
+  },
+  {
+    "revision": "450fbd7864f155edc303",
+    "url": "/weather/static/js/runtime-main.de668264.js"
+  },
+  {
+    "revision": "602aa2a3854c42a6b1cdbb01a57be067",
+    "url": "/weather/static/media/img.602aa2a3.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
